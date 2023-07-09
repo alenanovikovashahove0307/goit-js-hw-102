4c32f2a994035d3d8bb99aefdefc09560f4fa6d5
alenanovikovashahove0307 <novikova-fin@ukr.net>
2023-07-09 22:37:33 +0300

    Додано бібліотеки

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-
+import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
 axios.defaults.headers.common['x-api-key'] =
   'live_3DU0HxAKeUWlSlfcOosTj6D8mQ6FrmyjxFlajYL9WXY62yk84N5zW0o7K39AfWf2';
 
@@ -7,7 +8,7 @@ export function fetchBreeds() {
   return axios
     .get('https://api.thecatapi.com/v1/breeds')
     .then(response => response.data)
-    .catch(error => {
+      .catch(error => {
       console.error('Error fetching breeds:', error);
       throw error;
     });
--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 import { fetchBreeds, fetchCatByBreed, displayCatInfo } from './cat-api.js';
-
+import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
 // Завантажувач
 const loaderElement = document.querySelector('p.loader');
 
 // Помилка
 const errorElement = document.querySelector('p.error');
-
+const breedSelect = document.querySelector('select.breed-select');
+const catInfoContainer = document.querySelector('div.cat-info');
+const errorNotification = Notiflix.Notify.failure(errorElement.textContent);
+errorElement.style.display = 'none';
+breedSelect.style.display = 'none';
+new SlimSelect({
+  select: '#breedSelect',
+});
 // Завантаження колекції порід та заповнення select.breed-select
 fetchBreeds()
   .then(breeds => {
-    const breedSelect = document.querySelector('select.breed-select');
-    const catInfoContainer = document.querySelector('div.cat-info');
-
-    breeds.forEach(breed => {
+      breeds.forEach(breed => {
       const option = document.createElement('option');
       option.value = breed.id;
       option.textContent = breed.name;
       breedSelect.appendChild(option);
     });
-
+    
+    breedSelect.style.display = 'block';
     // Прослуховування події зміни вибраної породи
     breedSelect.addEventListener('change', event => {
       const selectedBreedId = event.target.value;
@@ -49,6 +55,34 @@ fetchBreeds()
     // Приховати завантажувач, показати помилку
     loaderElement.style.display = 'none';
     errorElement.style.display = 'block';
-
+    errorNotification.show();
     console.error('Failed to fetch breeds:', error);
+  })
+  .finally(() => {
+    loaderElement.style.display = 'none';
   });
+
+breedSelect.addEventListener('change', event => {
+  const selectedBreedId = event.target.value;
+
+  // Показати завантажувач, приховати select та div.cat-info
+  loaderElement.style.display = 'block';
+  breedSelect.style.display = 'none';
+  catInfoContainer.style.display = 'none';
+
+  fetchCatByBreed(selectedBreedId)
+    .then(catData => {
+      // Приховати завантажувач, показати div.cat-info
+      loaderElement.style.display = 'none';
+      catInfoContainer.style.display = 'block';
+
+      displayCatInfo(catData);
+    })
+    .catch(error => {
+      // Приховати завантажувач, показати помилку
+      
+      errorElement.style.display = 'block';
+      errorNotification.show();
+      console.error('Failed to fetch cat by breed:', error);
+    })
+});
